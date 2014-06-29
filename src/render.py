92fc9d867a69c75b97576399be0182c0767dd939
ubinexy <ubinexy@gmail.com>
2014-06-30 01:29:35 +0800

    complete render.py

--- src/render.py
+++ src/render.py
@@ -17,29 +17,34 @@ def footer():
 
 def render():
     count = 0
-    split = ''
+    split1 = ''
+    split2 = ''
 
     def change1(matched):
         return matched.group(1) + 'subroutine ' + matched.group(3) 
 
     def change2(matched):
         return matched.group(1) + 'call ' + matched.group(3)
-    
+
     for line in raw_text:
         if re.match('(\s*)(test\s+)(\w+\s*)', line):
             count += 1
             name1 = re.sub('(\s*)(test\s+)(\w+\s*)', change1, line)
             name2 = re.sub('(\s*)(test\s+)(\w+\s*)', change2, line)
-            split += name1
+            split1 += name1
+            split2 += name2
             continue
         if re.match('\s*end test\s*', line):
-            break
+            split1 += re.sub('(\s*end )(test)(\s*)', change1, line) + '\n'
+            count -= 1
+            continue
         if count > 0:
-            split += line
+            split1 += line
+        else:
+            pass
 
-    split += '\tend subroutine\n'
-    split += name2
-    return split
+    # split += name2
+    return (split2, split1)
 
 
 
@@ -72,9 +77,9 @@ raw_text = open('circle_class.fun')
 raw_tt = raw_text.read()
 raw_text = open('circle_class.fun')
 
-temp =  raw_tt.count('end test')
-
-for i in range(temp):
-    print render()
-
+# temp =  raw_tt.count('end test')
+a = render()
+# for i in range(temp):
+print a[0]
 print footer()
+print a[1]
